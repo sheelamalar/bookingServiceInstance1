5e88be10df69089a7d56e6fbdb23826f49acd362
Sheela Malar <Sheelamalar@gmail.com>
2023-05-24 09:43:05 +0530

    initial commit

---
+++ src/main/java/com/reservation/bookingService/model/Passenger.java
@@ -0,0 +1,41 @@
+package com.reservation.bookingService.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="passenger")
+public class Passenger {
+
+	@Id
+	@Column(name="passenger_id")
+	private int	passengerId;
+	
+	@Id
+	@Column(name="booking_number")
+	private int	bookingNumber;
+	
+	@Column(name="passenger_name")
+	private String passengerName;
+
+	public int getPassengerId() {
+		return passengerId;
+	}
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+	public int getBookingNumber() {
+		return bookingNumber;
+	}
+	public void setBookingNumber(int bookingNumber) {
+		this.bookingNumber = bookingNumber;
+	}
+	public String getPassengerName() {
+		return passengerName;
+	}
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+}
